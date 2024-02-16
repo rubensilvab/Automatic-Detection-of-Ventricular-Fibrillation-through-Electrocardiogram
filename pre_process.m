a2f7f55a3c29d719a237f0314e25e3790b24926d
Rúben Baeza <130314085+rubensilvab@users.noreply.github.com>
2024-02-16 23:16:44 +0000

    Update pre_process.m

--- pre_process.m
+++ pre_process.m
@@ -1,35 +1,42 @@
 function[ecgf,t]=pre_process(ecg1,fa)
-N=length(ecg1);
 
-%Retirar média do sinal 
-ecg1=ecg1-mean(ecg1);
+%Input: ecg1-> raw ecg signal
+  %     fa  -> sample frequency
+  
+%Output  ecgf-> preprocessed ECG
+%        t-> real time
 
-%filtro para tirar rede eletrica
+N = length(ecg1);
 
-r=1;
-teta=(60/fa)*2*pi; %omega=2pi*(f/fa)
-z1=r*exp(j*teta);% ao por o omega equivalente à freq 60 Hz , vai tornar fase do zero e sabemos que as fases do zero serão as freq com ganhos mais baixos 
-z2=r*exp(-j*teta);
-num=poly([z1,z2]);
-den=1;
-yecg1=filter(num,den,ecg1);  %filtro rejeita banda em torno de teta , 60 hz
+% Remove signal mean
+ecg1 = ecg1 - mean(ecg1);
 
-%FILTRO BASELINE WANDER FILTER
-N200=fa*0.200; %200ms amostras
-N600=fa*0.600; %600ms amostras
+% Filter to remove powerline interference
+r = 1;
+theta = (60/fa) * 2 * pi; % omega = 2pi * (f/fa)
+z1 = r * exp(j * theta); % setting omega equivalent to 60 Hz will nullify the phase
+z2 = r * exp(-j * theta);
+num = poly([z1, z2]);
+den = 1;
+yecg1 = filter(num, den, ecg1);  % bandstop filter around theta, 60 Hz
 
-L=1:N200:N+1; inic=L(1);
+% Baseline Wander Filter (removes low frequencies from moving, breathing, etc)
+N200 = fa * 0.200; % 200ms samples
+N600 = fa * 0.600; % 600ms samples
 
-for i=1:length(L);
-    yyecg1(inic:L(i)-1)=mean(yecg1(inic:L(i)-1));
-inic=L(i);
+L = 1:N200:N+1; inic = L(1);
+
+for i = 1:length(L)
+    yyecg1(inic:L(i)-1) = mean(yecg1(inic:L(i)-1));
+    inic = L(i);
 end
-L=1:N600:N+1; inic=L(1);
-for i=1:length(L);
-    yyyecg1(inic:L(i)-1)=mean(yyecg1(inic:L(i)-1));
-inic=L(i);
+
+L = 1:N600:N+1; inic = L(1);
+for i = 1:length(L)
+    yyyecg1(inic:L(i)-1) = mean(yyecg1(inic:L(i)-1));
+    inic = L(i);
 end
 
-ecgf=yecg1(1:length(yyyecg1))-yyyecg1; % retirar do sinal original
-Nf=length(ecgf);
-t=0:1/fa:(Nf-1)/fa;
+ecgf = yecg1(1:length(yyyecg1)) - yyyecg1; % Remove from the original signal
+Nf = length(ecgf);
+t = 0:1/fa:(Nf-1)/fa;
